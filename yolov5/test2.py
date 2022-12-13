13aac8b4ae9420cbbb62402d7f97b8d414eb0fa5
knippink <20110252@student.hcmute.edu.vn>
2022-12-13 12:30:07 +0700

    change fps

--- yolov5/test2.py
+++ yolov5/test2.py
@@ -204,10 +204,10 @@ def detect(opt, save_img=False, MSG_POKER = {}):
                             w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
                             h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
                         else:  # stream
-                            fps, w, h = 30, im0.shape[1], im0.shape[0]
+                            fps, w, h = 20, im0.shape[1], im0.shape[0]
                             save_path += '.mp4'
                         vid_writer = cv2.VideoWriter(
-                            save_path, cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), fps, (w, h))
+                            save_path, cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h))
                     vid_writer.write(im0)
 
     if save_txt or save_img:
